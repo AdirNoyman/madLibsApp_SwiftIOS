3f657ee48a9830ff8b74a39cecc99b057e85c922
Adir Noyman <45500652+AdirNoyman@users.noreply.github.com>
2020-05-02 22:17:01 +0300

    Add files via upload

---
+++ MadLibsApp/MadLibsApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MadLibsApp
+//
+//  Created by אדיר נוימן on 02/05/2020.
+//  Copyright © 2020 Adir Noyman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var verbTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var numberOfPetsLabel: UILabel!
+    @IBOutlet weak var animalSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var numberSlider: UISlider!
+    @IBOutlet weak var numberOfPetsStepper: UIStepper!
+    @IBOutlet weak var happyEndingSwitch: UISwitch!
+    
+    @IBOutlet weak var containerView: UIView!
+}
+
